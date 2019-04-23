c44cd9a40c0ea966f90fcab8145eccc039e0c566
bigmachini <elisha.wambiji@bigmachini.net>
2019-04-23 15:28:09 +0300

    comment on code for future

--- app/decorators.py
+++ app/decorators.py
@@ -2,24 +2,41 @@ from functools import wraps
 from flask import request, jsonify
 import datetime
 
-from app.models import App,Access
+from app.models import App, Access
 
 
 def app_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         app_id = request.headers.get('X-APP-ID')
-        # app_secret = request.headers.get('X-APP-SECRET')
-        # if app_id is None:
-        #     return jsonify({}), 403
+        app_token = request.headers.get('X-APP-TOKEN')
 
-        # if app_id != 'elisha' or app_secret != "mySecret":
-        #     return jsonify({}), 403
+        #Check if app_id or app_token exist
+        if app_id is None or app_token is None:
+            return jsonify({}), 403
+        
+        #check is app exists in database
+        app = App.objects.filter(app_id=app_id).first()
+        if not app:
+            return jsonify({}), 403
 
+        #check app access from database
+        access = Access.objects.filter(app=app).first()
+
+        #No access for app
+        if not access:
+            return jsonify({}), 403
+
+        #token invalid from issued
+        if access.token != app_token:
+            return jsonify({}), 403
+            
+        #token expired
+        if access.expires < datetime.datetime.now():
+            error = {
+                'code': 'TOKEN_EXPIRED'
+            }
+            return jsonify({'error': error}), 403
 
         return f(*args, **kwargs)
     return decorated_function
-
-
-     
- 
