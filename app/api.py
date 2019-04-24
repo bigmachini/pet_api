b7ef4752adfae4ac52c55e80ce50c59fe8f8295a
bigmachini <elisha.wambiji@bigmachini.net>
2019-04-24 14:46:01 +0300

    add tests to api

--- app/api.py
+++ app/api.py
@@ -24,7 +24,7 @@ class AppAPI(MethodView):
         existing_app = App.objects.filter(app_id=app_id).first()
         if existing_app:
             error = {
-                "code": "APP_ID_ALREADY_EXISTING"
+                "code": "APP_ID_ALREADY_EXISTS"
             }
             return jsonify({'error': error}), 400
         else:
@@ -53,14 +53,14 @@ class AccessAPI(MethodView):
         app = App.objects.filter(app_id=app_id).first()
         if not app:
             error = {
-                'code': 'INCORRECT_CREDENTIALS'
+                "code": "INCORRECT_CREDENTIALS"
             }
             return jsonify({'error': error}), 403
         else:
-            if bcrypt.hashpw((app_secret), app.app_secret):
+            if bcrypt.hashpw(app_secret, app.app_secret) == app.app_secret:
                 existing_tokens = Access.objects.filter(app=app).delete()
                 token = str(uuid.uuid4())
-                now = datetime.utcnow().replace(seconds=0, microsecond=0)
+                now = datetime.utcnow().replace(second=0, microsecond=0)
                 expires = now + timedelta(days=30)
 
                 access = Access(
@@ -76,3 +76,43 @@ class AccessAPI(MethodView):
                     'code': 'INCORRECT_CREDENTIALS'
                 }
                 return jsonify({'error': error}), 403
+                
+# class AccessAPI(MethodView):
+
+#     def __init__(self):
+#         if not request.json:
+#             abort(400)
+
+#     def post(self):
+#         if not "app_id" in request.json or not "app_secret" in request.json:
+#             error = {
+#                 "code": "MISSING_APP_ID_OR_APP_SECRET"
+#             }
+#             return jsonify({'error': error}), 400
+
+#         app = App.objects.filter(app_id=request.json.get('app_id')).first()
+#         if not app:
+#             error = {
+#                 "code": "INCORRECT_CREDENTIALS"
+#             }
+#             return jsonify({'error': error}), 403
+#         else:
+#             # generate a token
+#             if bcrypt.hashpw(request.json.get('app_secret'), app.app_secret) == app.app_secret:
+#                 # delete existing tokens
+#                 existing_tokens = Access.objects.filter(app=app).delete()
+#                 token = str(uuid.uuid4())
+#                 now = datetime.utcnow().replace(second=0, microsecond=0)
+#                 expires = now + timedelta(days=30)
+#                 access = Access(
+#                     app=app,
+#                     token=token,
+#                     expires=expires
+#                 ).save()
+#                 expires_3339 = expires.isoformat("T") + "Z"
+#                 return jsonify({'token': token, 'expires': expires_3339}), 200
+#             else:
+#                 error = {
+#                     "code": "INCORRECT_CREDENTIALS"
+#                 }
+#                 return jsonify({'error': error}), 403
