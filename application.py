d07eb598790a62a9c4f4a8f51f627c8732345800
bigmachini <elisha.wambiji@bigmachini.net>
2019-04-25 13:46:59 +0300

    finalize api

--- application.py
+++ application.py
@@ -1,28 +1,42 @@
 from flask import Flask
 from flask_mongoengine import MongoEngine
+
+from subprocess import call
+from settings import MONGODB_HOST
+
 db = MongoEngine()
 
 
 def create_app(**config_overrides):
     app = Flask(__name__)
 
-    #Load config
+    # Load config
     app.config.from_pyfile('settings.py')
 
-    #apply overrides for test
+    # apply overrides for test
     app.config.update(config_overrides)
 
-    #set up db
+    # set up db
     db.init_app(app)
 
     #import blueprints
     from home.views import home_app
     from pet.views import pet_app
     from app.views import app_app
+    from store.views import store_app
 
-    #register blueprints
+    # register blueprints
     app.register_blueprint(home_app)
     app.register_blueprint(pet_app)
     app.register_blueprint(app_app)
-    
+    app.register_blueprint(store_app)
+
     return app
+
+
+def fixtures(test_db, collection, fixture):
+    command = 'mongoimport -h {} \
+        -d {} \
+        -c {} \
+        < {}'.format(MONGODB_HOST, test_db, collection, fixture )
+    call(command, shell=True)
